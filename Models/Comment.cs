d2e762efc0e71dba56598633c013c736948c8f2c
Selahattin Demir <selahattin0739@gmail.com>
2023-04-22 01:17:48 +0300

    Modeller tanımlandı

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -2,5 +2,10 @@
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int Task_id { get; set; }
+        public string Comments { get; set; }
+        public DateTime Create_on { get; set; }
+        public int User_id { get; set; }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -2,5 +2,17 @@
 {
     public class Users
     {
-    }
+        public int Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Task_id { get; set; }
+        public int Assigning_the_job { get; set; }
+        public DateTime Create_on { get; set; }
+		public int active_task { get; set; }
+		public int End_on { get; set; }
+
+	}
+    
 }
