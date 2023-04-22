f2378fbc2a498658a8ebd8719b09df2665a6dc42
Selahattin Demir <selahattin0739@gmail.com>
2023-04-22 03:26:24 +0300

    Account controller eklendi,Login ve Signİn actionları tamamlandı Viewlar entegre edildi validationsişlemleri yapılacak

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using TaskTracking.Models;
+
+namespace TaskTracking.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly string connectionString = "Server=104.247.162.242\\MSSQLSERVER2017;Database=akadem58_tadimrehberi;User Id=akadem58_tr;Password=8C4ra!n07;TrustServerCertificate=True";
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UsersLogin login)
+        {
+          
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+
+
+                var command = new SqlCommand("select * from Users where email = @Email and password = @Password and is_confirm =@Is_confirm ", connection);
+                command.Parameters.AddWithValue("@Email", login.Email);
+                command.Parameters.AddWithValue("@Password", login.Password);
+                command.Parameters.AddWithValue("@@Is_confirm", login.Is_confirm);
+
+                var reader = command.ExecuteReader();
+
+                var pas = login.Password;
+                var mail = login.Email;
+                var verify = login.Is_confirm;
+
+                if (verify = true && mail=="@Email" && pas =="@Password") 
+                {
+                    return Content("1");
+                }
+                else
+                {
+                    return Content("0");
+                }
+            }
+
+                
+        }
+
+
+
+        public IActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserCreate create)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO Users (first_name,last_name,email,password,create_on, is_confirm) VALUES (@first_name,@last_name,@Email,@Password,@Create_on,@Is_confirm) ", connection);
+                command.Parameters.AddWithValue("@first_name", create.First_name);
+                command.Parameters.AddWithValue("@last_name", create.Last_name);
+                command.Parameters.AddWithValue("@Email", create.Email);
+                command.Parameters.AddWithValue("@Password", create.Password);
+                command.Parameters.AddWithValue("@Create_on", DateTime.Now);
+                command.Parameters.AddWithValue("@Is_confirm", false);
+
+
+                command.ExecuteNonQuery();
+            }
+
+                return Content("1");
+        }
+    }
+
+    //Selahattin
+
+    
+}
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskTracking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracking.Models
 {
     public class Users
     {
@@ -12,7 +14,37 @@
         public DateTime Create_on { get; set; }
 		public int active_task { get; set; }
 		public int End_on { get; set; }
+        public bool Is_confirm { get; set;}
 
 	}
+    public class UserCreate
+    {
+        [Required]
+        public string First_name { get; set; }
+
+        [Required]
+        public string Last_name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool Is_confirm { get; set; }
+
+        public DateTime Create_on { get; set;}
+    }
+    public class UsersLogin
+    {
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool Is_confirm { get; set; }
+    }
     
 }
